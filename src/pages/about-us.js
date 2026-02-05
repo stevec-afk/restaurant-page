58a067093d99a83089521c071fd3b0f64815794f
stevec-afk <evets25@gmail.com>
2026-02-05 15:43:58 -0400

    created about us page in html

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,7 +1,15 @@
 import { $content, createHtmlElement } from "../index.js";
 
 function render() {
-    return;
+    const $logo = createHtmlElement("div", undefined, undefined, "logo");
+    const $h2 = createHtmlElement("h2", "Steve's", ["small"]);
+    const $h1 = createHtmlElement("h1", "Catering", ["small"]);
+    const $menu = createHtmlElement("div", undefined, undefined, "menu")
+
+    $content.innerHTML = ""; // Clear content div
+    $logo.appendChild($h2);
+    $logo.appendChild($h1);
+    $content.appendChild($logo);
 }
 
 export {render as renderAboutus};
