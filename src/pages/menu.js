bfc83b7d970c83d4e39f24c15f181dd47bff5b7e
stevec-afk <evets25@gmail.com>
2026-02-09 16:26:32 -0400

    Comment cleanup

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,18 +1,6 @@
 import { $content, createHtmlElement } from "../index.js";
 import {renderLogo} from "./home.js";
 
-// createHtmlElement("type", "content", [classes], "id")
-
-// function renderLogo() { 
-//     const $logo = createHtmlElement("div", undefined, undefined, "logo");
-//     const $h2 = createHtmlElement("h2", "Steve's", ["small"]);
-//     const $h1 = createHtmlElement("h1", "Catering", ["small"]);
-
-//     $logo.appendChild($h2);
-//     $logo.appendChild($h1);
-//     $content.appendChild($logo);
-// }
-
 function renderMenu() {
     renderLogo(true); 
 
@@ -40,12 +28,10 @@ function renderMenu() {
         const $div = createHtmlElement("div", undefined, ["menu-item"]);
         const $name = createHtmlElement("h1", item.name);
         const $description = createHtmlElement("p", item.description);
-        // const $price = createHtmlElement("h2", item.price);
         const $hr = createHtmlElement("hr", undefined, ["breakline"]);
 
         $div.appendChild($name);
         $div.appendChild($description);
-        // $div.appendChild($price);
         $div.appendChild($hr);
         $menu.appendChild($div);
     });
